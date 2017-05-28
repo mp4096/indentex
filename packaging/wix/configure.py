dc5d7c4ec541fe73c00230524091179fc8ab8a47
Hans Kirchner <hans-kirchner@gmx.de>
2017-05-28 17:07:17 +0200

    Fix unacceptable PEP8 issues in configure.py

--- packaging/wix/configure.py
+++ packaging/wix/configure.py
@@ -2,17 +2,20 @@ from os import path
 from string import Template
 import toml
 
+
 def get_version():
     with open(path.join("..", "..", "Cargo.toml")) as f:
         cargo = toml.load(f)
         return cargo["package"]["version"]
 
-def configure_wxs_file(version):
+
+def configure_wxs_file(**context):
     with open("indentex_template.wxs") as f:
         tmpl = Template(f.read())
 
     with open("indentex.wxs", "w") as f:
-        f.write(tmpl.substitute(version=version))
+        f.write(tmpl.substitute(context))
+
 
 if __name__ == "__main__":
-    configure_wxs_file(get_version())
+    configure_wxs_file(version=get_version())
