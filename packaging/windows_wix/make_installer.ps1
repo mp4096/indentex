1b3829ea7c38cdd42ba2002cfe582b0b069c8f9a
Hans Kirchner <hans-kirchner@gmx.de>
2017-05-31 15:56:02 +0200

    Use even more Powershell. Remove configure.py

--- packaging/windows_wix/make_installer.ps1
+++ packaging/windows_wix/make_installer.ps1
@@ -1,12 +1,17 @@
 cargo build --release --target=x86_64-pc-windows-msvc
 Copy-Item ..\..\target\x86_64-pc-windows-msvc\release\indentex.exe .
 
-python configure.py
+$PackageVersion = $(python package_info.py)
+
+(Get-Content .\indentex_template.wxs) `
+    -replace '{{{version}}}', "$PackageVersion" `
+    | Set-Content .\indentex.wxs
+
 pandoc -f markdown -t rtf ..\..\LICENSE.md -s -o LICENSE.rtf
 
 & "$env:WIX\bin\candle" indentex.wxs
 & "$env:WIX\bin\light" indentex.wixobj -ext WixUIExtension -sice:ICE91
 
-$TargetFile = "indentex_$(python package_info.py)_amd64.msi"
+$TargetFile = "indentex_${PackageVersion}_amd64.msi"
 Remove-Item $TargetFile -ErrorAction Ignore
 Rename-Item indentex.msi $TargetFile
