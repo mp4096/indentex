e918c46603acf811a3428200cc9be4d7fca7d0cb
Hans Kirchner <hans-kirchner@gmx.de>
2017-05-31 14:55:43 +0200

    Use more powershell

--- packaging/windows_wix/make_installer.ps1
+++ packaging/windows_wix/make_installer.ps1
@@ -1,5 +1,5 @@
 cargo build --release --target=x86_64-pc-windows-msvc
-cp ..\..\target\x86_64-pc-windows-msvc\release\indentex.exe .
+Copy-Item ..\..\target\x86_64-pc-windows-msvc\release\indentex.exe .
 
 python configure.py
 pandoc -f markdown -t rtf ..\..\LICENSE.md -s -o LICENSE.rtf
@@ -7,4 +7,6 @@ pandoc -f markdown -t rtf ..\..\LICENSE.md -s -o LICENSE.rtf
 & "$env:WIX\bin\candle" indentex.wxs
 & "$env:WIX\bin\light" indentex.wixobj -ext WixUIExtension -sice:ICE91
 
-python rename_installer.py
+$TargetFile = "indentex_$(python package_info.py)_amd64.msi"
+Remove-Item $TargetFile -ErrorAction Ignore
+Rename-Item indentex.msi $TargetFile
--- packaging/windows_wix/package_info.py
+++ packaging/windows_wix/package_info.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from os import path
 import toml
 
@@ -6,3 +7,7 @@ def get_version():
     with open(path.join("..", "..", "Cargo.toml")) as f:
         cargo = toml.load(f)
         return cargo["package"]["version"]
+
+
+if __name__ == "__main__":
+    print(get_version())
