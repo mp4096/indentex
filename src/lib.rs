3b833ac32b6da2d73dec51b7583148bafa8b6d10
Hans Kirchner <hans-kirchner@gmx.de>
2017-06-11 02:05:08 +0200

    Simplify api implementation. Add flags enum

--- src/lib.rs
+++ src/lib.rs
@@ -17,22 +17,14 @@ mod transpile;
 use std::ffi::CStr;
 use std::os::raw::c_char;
 
+enum TranspileFlags {
+    FlattenOutput    = 0b01,
+    DisableDoNotEdit = 0b10,
+}
+
 #[no_mangle]
 pub extern fn indentex_transpile_file(path: *const c_char) -> i32 {
-    use transpile::{transpile_file, TranspileOptions};
-
-    let rust_path = unsafe { CStr::from_ptr(path).to_string_lossy().into_owned() };
-
-    return match transpile_file(rust_path.as_str(), &TranspileOptions {
-        flatten_output: false,
-        prepend_do_not_edit_notice: false,
-    }) {
-        Ok(_) => 0,
-        Err(e) => {
-            println!("Could not transpile '{}': {}", rust_path, e);
-            1
-        }
-    };
+    return indentex_transpile_file_options(path, 0);
 }
 
 #[no_mangle]
@@ -42,8 +34,8 @@ pub extern fn indentex_transpile_file_options(path: *const c_char, options: i32)
     let rust_path = unsafe { CStr::from_ptr(path).to_string_lossy().into_owned() };
 
     return match transpile_file(rust_path.as_str(), &TranspileOptions {
-        flatten_output: options & 1 != 0,
-        prepend_do_not_edit_notice: options & 2 == 0,
+        flatten_output: options & TranspileFlags::FlattenOutput as i32 != 0,
+        prepend_do_not_edit_notice: options & TranspileFlags::DisableDoNotEdit as i32 == 0,
     }) {
         Ok(_) => 0,
         Err(e) => {
