b98dee5746446eff40dc75fd264f8385faf0ef48
Hans Kirchner <hans-kirchner@gmx.de>
2017-06-11 16:58:53 +0200

    Add python test script for libindentex.so

---
+++ tests/test_libindentex.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from cffi import FFI
+
+INPUT = b"""# equation*:
+  # label: eq:test
+  a + b"""
+
+def main():
+    ffi = FFI()
+    ffi.cdef("""
+    int indentex_transpile_flags(const void *input, size_t input_len,
+        void *output, size_t output_len, int flags);
+    """)
+    indentex = ffi.dlopen("target/debug/libindentex.so")
+
+    buf_len = 1000
+    buf = ffi.new('char[]', buf_len)
+
+    ret_val = indentex.indentex_transpile_flags(INPUT, len(INPUT), buf, buf_len, 0)
+
+    print(ret_val)
+    print(ffi.string(buf).decode("utf-8"))
+
+
+if __name__ == "__main__":
+    main()
