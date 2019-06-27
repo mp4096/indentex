95b067aad4ec3c11dc4a7bc738b3fc24e7690a68
Mikhail Pak <code@mp4096.com>
2019-06-27 20:55:43 +0200

    Add an integration test for vanilla LaTeX
    Test the superset property

--- indentexlib/tests/integration_test.rs
+++ indentexlib/tests/integration_test.rs
@@ -1,14 +1,21 @@
 use insta::assert_display_snapshot_matches;
 
 #[cfg(test)]
-fn transpile_from_file(test_filename: &str) -> String {
+fn read_from_file(test_filename: &str) -> Vec<String> {
     let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     let test_case_path: std::path::PathBuf = [&manifest_dir, "tests", "assets", test_filename]
         .iter()
         .collect();
     let test_case_file = std::fs::File::open(test_case_path).unwrap();
     let test_case_buf = std::io::BufReader::new(test_case_file);
-    let lines = indentexlib::preprocessing::read_and_trim_lines(test_case_buf).unwrap();
+    let lines = indentexlib::preprocessing::read_and_trim_lines(test_case_buf);
+    assert!(lines.is_ok());
+    lines.unwrap()
+}
+
+#[cfg(test)]
+fn transpile_from_file(test_filename: &str) -> String {
+    let lines = read_from_file(test_filename);
 
     let default_options = indentexlib::TranspileOptions {
         flatten_output: false,
@@ -16,6 +23,22 @@ fn transpile_from_file(test_filename: &str) -> String {
     };
 
     indentexlib::transpile(lines, &default_options)
+}
+
+#[test]
+fn vanilla_latex_is_not_modified() {
+    use indentexlib::preprocessing::read_and_trim_lines;
+    use std::io::BufReader;
+
+    let lines = read_from_file("large_latex_corpus.inden.tex");
+    let expected_lines = lines.clone();
+    let no_flatten_no_prepend = indentexlib::TranspileOptions {
+        flatten_output: false,
+        prepend_do_not_edit_notice: false,
+    };
+    let transpiled = indentexlib::transpile(lines, &no_flatten_no_prepend);
+    let actual_lines = read_and_trim_lines(BufReader::new(transpiled.as_bytes())).unwrap();
+    assert_eq!(actual_lines, expected_lines);
 }
 
 #[test]
